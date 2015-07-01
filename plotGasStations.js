c0eace34d8553061bf820e651d027e44d358a095
Biniam Asnake <biniam.asnake@kaufda.de>
2015-07-01 17:40:47 +0200

    plotGasStations.js modified

--- plotGasStations.js
+++ plotGasStations.js
@@ -9,8 +9,8 @@ function plotGasStations() {
         popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
     });
 
-    var petrolStationOne = L.marker([44.439727, 25.926222], {icon: gasStationIcon}).bindPopup('Station One'),
-        petrolStationTwo = L.marker([44.438134, 25.945062], {icon: gasStationIcon}).bindPopup('Station Two');
+    var petrolStationOne = L.marker([44.506279, 25.987052], {icon: gasStationIcon}).bindPopup('Station One'),
+        petrolStationTwo = L.marker([44.487162, 26.018423], {icon: gasStationIcon}).bindPopup('Station Two');
 
     var petrolStations = L.layerGroup([petrolStationOne, petrolStationTwo]);
 
