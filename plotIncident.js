203fd4c95a2ea811b5565e94c8cf0746fc7d0fc7
Biniam Asnake <biniam.asnake@kaufda.de>
2015-07-01 22:37:28 +0200

    conflicts merged and test is successful. Ready for demo.

--- plotIncident.js
+++ plotIncident.js
@@ -6,14 +6,7 @@ function plotWarning() {
         popupAnchor:  [-3, -76]
     });
 
-<<<<<<< HEAD
     var warningOne = L.marker([44.510572, 26.007994], {icon: warningIcon}).addTo(map);
 
     warningOne.bindPopup("<b>Warning</b><br>Accident has been reported in this road. Please slow down.").openPopup();
 }
-=======
-    var warningOne = L.marker([44.508403, 26.005945], {icon: warningIcon}).addTo(map);
-
-    warningOne.bindPopup("<b>Warning</b><br>A Texas grandmother, in a horrific crash that trapped her inside her car for two days, survived two days by soaking her T-shirt in a nearby pond to get water.");
-}
->>>>>>> 6a52f9a703ce43880dcae916d16d756157ceab28
